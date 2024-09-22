4043a4ba26d54f679a808ec1570d6f02d1d8d952
Paradox10101 <a20202467@pucp.edu.pe>
2024-09-21 22:12:43 -0500

    Ejemplo de etiquetas faltantes en "Cliente" Model
    Esas cosas se deberian colocar en todos los modelos, sino no confirman.

--- backend/src/main/java/com/example/backend/models/Cliente.java
+++ backend/src/main/java/com/example/backend/models/Cliente.java
@@ -1,8 +1,25 @@
-
 package com.example.backend.models;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+@Entity //estas cosas siempre se debe colocar
 public class Cliente {
+    @Id
+    @GeneratedValue
     private int id_cliente;
+    
+    @Column(nullable = false)
     private String codigo;
     private String nombres;
     private String apellidos;
